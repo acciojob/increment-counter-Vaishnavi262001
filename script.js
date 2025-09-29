b138b6744f60693fef1e9f74929cf26db0f32e23
Vaishnavi262001 <164137331+Vaishnavi262001@users.noreply.github.com>
2025-09-30 00:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const status = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", () => {
+  // Insert an <h1> inside the <p> with id="status"
+  status.innerHTML = "<h1>Entered Metaverse</h1>";
+});
